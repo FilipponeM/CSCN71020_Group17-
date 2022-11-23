c2bee5d7cefba9e32c91f7d20dfb4e7f9d50cbc9
Matteo <mfilippone5127@conestogac.on.ca>
2022-11-23 10:36:12 -0500

    Added test projects

---
+++ PolygonTests/PolygonTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace PolygonTests
+{
+	TEST_CLASS(PolygonTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
---
+++ triangleAngleCalculatorTest/triangleAngleCalculatorTest.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace triangleAngleCalculatorTest
+{
+	TEST_CLASS(triangleAngleCalculatorTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
